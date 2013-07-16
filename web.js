d9539a3b77396278363188d4056bcf11e0bb982d
Cuong Tran <cuongtranuk@gmail.com>
2013-07-16 20:17:45 +0000

    Edit code 1

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var bf = require('buffer');
+var output = Buffer(256);
 
 var app = express.createServer(express.logger());
-
+fs.readFileSync('index.html', , function(err,data)) {
+	if(err) throw err;
+	output = data;
+});
 app.get('/', function(request, response) {
-  response.send('Hello World 222!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
