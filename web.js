900b1e41162b127ff09203966b9d72592fbba0a4
Cuong Tran <cuongtranuk@gmail.com>
2013-07-16 20:42:20 +0000

    Code edited 2

--- web.js
+++ web.js
@@ -4,10 +4,12 @@ var bf = require('buffer');
 var output = Buffer(256);
 
 var app = express.createServer(express.logger());
-fs.readFileSync('index.html', , function(err,data)) {
+fs.readFile('index.html','utf8', function(err, data) {
 	if(err) throw err;
-	output = data;
+	var buf = new Buffer(data, 'utf8');
+	console.log(buf.toString('utf8'));
 });
+/*
 app.get('/', function(request, response) {
   response.send(output);
 });
@@ -16,3 +18,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+*/
